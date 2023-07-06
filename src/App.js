07b95b6a7881c071b2e1df21b1052933edccb999
Afra <nurafraciftci@gmail.com>
2023-07-06 17:05:04 +0200

    v2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.scss";
 import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+
+
 
 function App() {
   return (
     <div className="App">
+     <Navbar></Navbar>
     <Header></Header>
     </div>
   );
